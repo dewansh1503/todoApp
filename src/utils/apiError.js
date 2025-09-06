bbfc7a372c68eb60d3dd0589d1762fbd8daa2225
Dewansh Pandey <dewansh1503@gmail.com>
2025-09-06 21:50:32 +0530

    adding apiError util

---
+++ src/utils/apiError.js
@@ -0,0 +1,9 @@
+class apiError extends Error {
+   constructor(statusCode, message) {
+      super(message);
+      this.statusCode = statusCode;
+      this.success = false;
+   }
+}
+
+export { apiError };
