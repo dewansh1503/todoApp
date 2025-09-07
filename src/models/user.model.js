f527cc0ac9bcbec02680e223fccd30881ae021fd
Dewansh Pandey <dewansh1503@gmail.com>
2025-09-07 22:38:47 +0530

    added generating access token method to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,3 +22,10 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isPasswordCorrect = function (password) {
    return bcrypt.compareSync(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+   const token = jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+   });
+   return token;
+};
