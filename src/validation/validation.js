c47cba8c96163828a0fc23319568b88727ea9c9a
Dewansh Pandey <dewansh1503@gmail.com>
2025-09-09 22:25:11 +0530

    sign up input validation added

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -9,3 +9,12 @@ const signinValidation = function (body) {
    const result = baseSchema.safeParse(body);
    return result;
 };
+
+const userSchema = baseSchema.extend({
+   username: z.string().trim().min(3).max(50),
+});
+
+const signupValidation = function (body) {
+   const result = userSchema.safeParse(body);
+   return result;
+};
