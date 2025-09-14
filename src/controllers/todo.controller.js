a252a00b353b196d2c36591547d26e86ed3c8aa8
Dewansh Pandey <dewansh1503@gmail.com>
2025-09-14 14:21:14 +0530

    adding done todo controller

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -34,3 +34,24 @@ const deleteTodo = asynchandler(async (req, res) => {
 
    res.status(200).json(new apiResponse(200, 'Todo deleted'));
 });
+
+const doneTodo = asynchandler(async (req, res) => {
+   const result = objectIdValidation(req.query);
+   if (result.error) {
+      const err = z.treeifyError(result.error);
+      const errMessageObject = JSON.stringify(err.properties);
+      throw new apiError(400, errMessageObject);
+   }
+
+   const { todoId } = result.data;
+   const todo = await Todo.findById(todoId);
+   if (!todo) throw new apiError(404, 'Todo did not exit');
+
+   // user didnt own the todo (even if the todo exits)
+   if (todo.userId != req.user._id) throw new apiError(400, 'Todo not found');
+
+   todo.done = true;
+   await todo.save();
+
+   res.status(200).json(new apiResponse(200, 'Done todo'));
+});
