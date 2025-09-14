fce1bc77747efe689d9f68365daae97f0995378e
Dewansh Pandey <dewansh1503@gmail.com>
2025-09-14 14:20:00 +0530

    adding delete todo controller

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -14,3 +14,23 @@ const createTodo = asynchandler(async (req, res) => {
    });
    res.status(200).json(new apiResponse(200, 'Task added', todo));
 });
+
+const deleteTodo = asynchandler(async (req, res) => {
+   const result = objectIdValidation(req.query);
+   if (result.error) {
+      const err = z.treeifyError(result.error);
+      const errMessageObject = JSON.stringify(err.properties);
+      throw new apiError(400, errMessageObject);
+   }
+
+   const { todoId } = result.data;
+   const todo = await Todo.findById(todoId);
+   if (!todo) throw new apiError(404, 'Todo did not exist');
+
+   // user didn't own todo (even if the todo exits)
+   if (todo.userId != req.user._id) throw new apiError(404, 'Todo not found');
+
+   const delObj = await Todo.findByIdAndDelete(todo._id);
+
+   res.status(200).json(new apiResponse(200, 'Todo deleted'));
+});
